76e2f75d865381d337555006a354f0e1fe9aa255
Lee Dale <lee.dale@ofqual.gov.uk>
2020-04-17 14:02:21 +0100

    Submitted version

--- start.ps1
+++ start.ps1
@@ -6,6 +6,9 @@ cd ../
 cd ./ApTap.Services.BusinessService
 docker build -t businessservice .
 cd ../..
+cd ./client/aptap-client
+docker build -t clientservice .
+cd ../..
 
 Write-Host "Build finished." -ForegroundColor Green
 
@@ -13,6 +16,7 @@ Write-Host "Starting Services..." -ForegroundColor Green
 
 docker run -d -p 5000:80 --name AuthService authservice --network=bridge
 docker run -d -p 5002:80 --name BusinessService businessservice --network=bridge
+docker run -d -p 3000:80 --name ClientService clientservice
 
 Write-Host "Services started." -ForegroundColor Green
 
